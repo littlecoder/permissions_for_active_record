1e01743872dd02de1e0f03fd1cbe3cd9b2708188
Malte Büchmann <buechmann@ubilabs.net>
2009-05-12 11:17:23 +0200

    Added permissions for associated objects.

--- lib/permissions_for_active_record.rb
+++ lib/permissions_for_active_record.rb
@@ -7,12 +7,62 @@ module AddPermissions
 
     module ClassMethods
       def add_permissions
+        
         before_create :creatable?
         before_save :updateable?
         before_destroy :destroyable?
         
         include AddPermissions::Permissions::InstanceMethods
         extend AddPermissions::Permissions::SingletonMethods
+        
+        #
+        # add permissions to associations
+        #
+
+        # loop over all associations
+        self.reflect_on_all_associations.each do |asoc|
+          # check if the associated class already has creatable_by_association? (set by another ActiveRecord with permissions)
+          unless defined? asoc.klass.saveable_by_association?
+            # add a new before_save filter to this ActiveRecord and the method creatable_by_association?
+            asoc.klass.class_eval do
+              def saveable_by_association?
+                res = true
+                
+                # check only the changed methods
+                self.changed.each do |c|
+                  # get the association for this method
+                  a1 = self.class.reflect_on_all_associations.detect do |a|
+                    a.primary_key_name == c
+                  end
+                  
+                  # if the changed method has an association
+                  if a1
+                    # get the associated ActiveRecord
+                    ao = self.method(a1.name).call
+                    # get the ActiveRecord's corresponding association
+                    a2 = ao.class.reflect_on_all_associations.detect do |a|
+                      a.primary_key_name == c and a.klass == self.class
+                    end
+                    # check if the method of the associated object for this ActiveRecord is updateable
+                    if ao
+                      begin
+                        res &= ao.updateable?(a2.name)
+                      rescue
+                      end
+                    end
+                    errors.add_to_base 'The ' + self.class.name + ' associated with ' + ao.class.name + ' is not permitted to be saved.' unless res
+                  end
+                end
+                
+                res
+              end
+              
+              # activate the before_save filter
+              before_save :saveable_by_association?
+            end
+          end
+        end
+        
       end
     end
 
@@ -26,7 +76,7 @@ module AddPermissions
         true
       end
         
-      def updateable?
+      def updateable? method = nil
         true
       end
         
